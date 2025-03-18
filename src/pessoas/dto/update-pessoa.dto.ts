ba5f4827d62c0f84ccfe7d0f7b631750c12a76f4
João Marcelo Carvalho Pereira Araújo <joaomarcelocpa0303@gmail.com>
2025-03-18 15:49:47 -0300

    Delete src directory

--- src/pessoas/dto/update-pessoa.dto.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePessoaDto } from './create-pessoa.dto';
-
-export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {
-}
