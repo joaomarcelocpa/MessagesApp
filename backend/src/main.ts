64c11e52182a86232cf5faf8f656968eab39c486
joaomarcelocpa <joaomarcelocpa0303@gmail.com>
2025-06-15 18:47:46 -0300

    cors backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,18 @@
 /* eslint-disable */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true, transform: true,}),);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+  });
+
+  await app.listen(3001);
+  console.log('Backend rodando em: http://localhost:3001');
 }
 bootstrap();
